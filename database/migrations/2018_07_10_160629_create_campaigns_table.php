a6fb3c4691ec8d348f9b277ee33ff8b4fe4b7451
Brian Gibbins <brian@zen1210.com>
2018-07-10 12:24:54 -0400

    EPCM-231 adds campaigns table migration

---
+++ database/migrations/2018_07_10_160629_create_campaigns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->integer('org_id');
+            $table->text('type');
+            $table->integer('status');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaigns');
+    }
+}
