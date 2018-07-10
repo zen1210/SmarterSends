0578ac9fd0c396ff27867f1942c56d1428d961b8
Brian Gibbins <brian@zen1210.com>
2018-07-10 12:41:53 -0400

    EPCM-231 adds schedule table migration

---
+++ database/migrations/2018_07_10_160651_create_schedule_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('day');
+            $table->text('type');
+            $table->text('gmail');
+            $table->text('hotmail');
+            $table->text('yahoo');
+            $table->text('aol');
+            $table->text('comcast');
+            $table->text('apple');
+            $table->text('default');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule');
+    }
+}
