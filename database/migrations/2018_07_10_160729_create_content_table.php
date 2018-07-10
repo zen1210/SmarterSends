43357d46a02665c03f24785ed08d7b7b9b206c2b
Brian Gibbins <brian@zen1210.com>
2018-07-10 12:27:16 -0400

    EPCM-231 adds content table migration

---
+++ database/migrations/2018_07_10_160729_create_content_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('org_id');
+            $table->integer('campaign_id');
+            $table->integer('edm_id');
+            $table->text('html');
+            $table->text('text');
+            $table->text('segment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+}
